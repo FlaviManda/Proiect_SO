27e8887f6c50fcf0677af3a7f158af0ec733141b
FlaviManda <127722043+FlaviManda@users.noreply.github.com>
2023-11-19 16:15:41 +0200

    Update proiect1.c

--- proiect1.c
+++ proiect1.c
@@ -7,129 +7,153 @@
 #include <string.h>
 #include <sys/stat.h>
 
+
 int main(int argc, char *argv[])
-{    
+{
     int width;
     int height;
     int dimensiune;
-    char *input=argv[1];
-    char buff[1000];
-    char multipleacc[3]="";
+    int user;
+    int control_leg;
+
+    if (argc != 2)
+    {
+        perror("eroare argumente");
+        exit(EXIT_FAILURE);
+    }
+
+    char *input = argv[1];
+    char buff[3000];
+    char multipleacc[3] = "";
     char useracc[3];
-    int bmp_fd=open(input, O_RDONLY);
+
+    int out = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (out == -1)
+    {
+        perror("eroare deschidere fisier de iesire");
+        exit(EXIT_FAILURE);
+    }
+
+    int bmp_fd = open(input, O_RDONLY);
+    if (bmp_fd == -1)
+    {
+        perror("eroare deschidere");
+        exit(EXIT_FAILURE);
+    }
 
     struct stat size;
 
-    int out=open("statistica.txt",O_WRONLY);
-    if( bmp_fd == -1)
+    if (stat(input, &size) == -1)
     {
-        perror("eroare deschidere");
-        exit(-1);
-    }
-    
-    lseek(bmp_fd, 18, SEEK_SET);
-    read(out, &width, sizeof(int));
-    lseek(bmp_fd, 22, SEEK_SET);
-    read(out, &height, sizeof(int));
-
-    if( stat("image.bmp", &size)==-1 )
-        {
         perror("eroare stat");
-        exit(-1);
-        }
-    
-    dimensiune=size.st_size;
+        exit(EXIT_FAILURE);
+    }
 
-    int user=size.st_uid;
-    
-    int control_leg=size.st_nlink;
+    lseek(bmp_fd, 18, SEEK_SET);
+    read(bmp_fd, &width, sizeof(int));
+    lseek(bmp_fd, 22, SEEK_SET);
+    read(bmp_fd, &height, sizeof(int));
 
-    if(size.st_mode & S_IRWXU)
+    dimensiune = size.st_size;
+    user = size.st_uid;
+    control_leg = size.st_nlink;
+
+    if (size.st_mode & S_IRWXU)
     {
-        useracc[0]='R';
+        useracc[0] = 'R';
     }
-    else{
-        useracc[0]='-';
-    }
-
-    if(size.st_mode & S_IWUSR)
+    else
     {
-        useracc[1]='w';
-    }
-    else{
-        useracc[1]='-';
+        useracc[0] = '-';
     }
 
-    if(size.st_mode & S_IXUSR)
+    if (size.st_mode & S_IWUSR)
     {
-        useracc[2]='E';
+        useracc[1] = 'W';
     }
-    else{
-        useracc[2]='-';
-    }
-
-    
-    if(size.st_mode & S_IRGRP)
+    else
     {
-        multipleacc[0]='R';
+        useracc[1] = '-';
     }
 
-    else{
-        multipleacc[0]='-';
-    }
-
-    if(size.st_mode & S_IWGRP )
+    if (size.st_mode & S_IXUSR)
     {
-        multipleacc[1]='W';
+        useracc[2] = 'E';
     }
-    else{
-        multipleacc[1]='-';
+    else
+    {
+        useracc[2] = '-';
     }
 
-    if(size.st_mode & S_IXGRP)
+    if (size.st_mode & S_IRGRP)
     {
-        multipleacc[2]='E';
+        multipleacc[0] = 'R';
     }
-    else{
-        multipleacc[2]='-';
-    }
-        char others[3]="";
-        if(size.st_mode & S_IROTH)
+    else
     {
-        others[0]='R';
-    }
-    else{
-        others[0]='-';
+        multipleacc[0] = '-';
     }
 
-    if(size.st_mode & S_IWOTH  )
+    if (size.st_mode & S_IWGRP)
     {
-        others[1]='W';
+        multipleacc[1] = 'W';
     }
-    else{
-        others[1]='-';
+    else
+    {
+        multipleacc[1] = '-';
     }
 
-    if(size.st_mode & S_IROTH)
+    if (size.st_mode & S_IXGRP)
     {
-        others[2]='E';
+        multipleacc[2] = 'E';
     }
-    else{
-        others[2]='-';
+    else
+    {
+        multipleacc[2] = '-';
     }
-        
+    char others[3] = "";
+    if (size.st_mode & S_IROTH)
+    {
+        others[0] = 'R';
+    }
+    else
+    {
+        others[0] = '-';
+    }
 
-    sprintf(buff,"nume fisier: %s\n inaltime: %d\n, lungime: %d\n, dimensiune: %d\n, identificarea utilizatorului: %d\n, numar legaturi: %d\n, drepturi de acces user: %s\n, drepturi de acces multiplu: %s\n, drepturi de acces others: %s\n", argv[1], height, width, dimensiune, user, control_leg, useracc, multipleacc, others);
-    
+    if (size.st_mode & S_IWOTH)
+    {
+        others[1] = 'W';
+    }
+    else
+    {
+        others[1] = '-';
+    }
+
+    if (size.st_mode & S_IROTH)
+    {
+        others[2] = 'E';
+    }
+    else
+    {
+        others[2] = '-';
+    }
+
+    sprintf(buff, "nume fisier: %s\n inaltime: %d\n, lungime: %d\n, dimensiune: %d\n, identificarea utilizatorului: %d\n, numar legaturi: %d\n, drepturi de acces user: %s\n, drepturi de acces multiplu: %s\n, drepturi de acces others: %s\n", argv[1], height, width, dimensiune, user, control_leg, useracc, multipleacc, others);
+
     write(out, buff, strlen(buff));
-    
-    if(close(bmp_fd)==-1)
-        {
-            perror("error close");
-            exit(1);
-        }
-        
-    close(out);
+
+    if (close(bmp_fd) == -1)
+    {
+        perror("error close");
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(out) == -1)
+    {
+        perror("error close");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
